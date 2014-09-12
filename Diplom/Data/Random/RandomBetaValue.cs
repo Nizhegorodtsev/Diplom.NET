ee6d3dc8a986230bb4ea5bf8852213313b8ae01e
nizhegorodtsev <nizhegorodtsev@ligaforex.com>
2014-09-12 20:23:32 +0700

    Начал делеать бета распределение

---
+++ Diplom/Data/Random/RandomBetaValue.cs
@@ -0,0 +1,95 @@
+﻿using Diplom.Data.Exeption;
+using Diplom.Data.Utilities;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplom.Data.Random
+{
+    class RandomBetaValue : AbstractRandomValue
+    {
+        private double alpha;
+
+        private double beta;
+
+        private RandomBasicValue basicRandom;
+
+        private List<Double> forIntegerGenerate = null;
+
+        public bool isIntegerType;
+
+        public static readonly String ALPHA = "Alpha"; 
+
+        public static readonly String BETA = "Beta";
+
+        public RandomBetaValue()
+        {
+            basicRandom = new RandomBasicValue();
+        }
+
+        public override double nextValue()
+        {
+            if (isIntegerType)
+                return nextInteger();
+            return nextFractional();
+        }
+
+        private double nextInteger()
+        {
+            forIntegerGenerate.Clear();
+            Double x=0;
+            for (int i = 0; i < alpha + beta + 1; i++) 
+            {
+                x = basicRandom.nextValue();
+                int j=0;
+                while (j < forIntegerGenerate.Count && x > forIntegerGenerate[j]) 
+                {
+                    j++;
+                }
+                if (j < forIntegerGenerate.Count)
+                    forIntegerGenerate.Insert(j, x);
+                else
+                    forIntegerGenerate.Add(x);
+            }
+            return forIntegerGenerate[(int)alpha];
+        }
+
+        private double nextFractional()
+        {
+
+        }
+        
+        public override JObject store()
+        {
+           JObject state=  base.store();
+           state.Add(ALPHA, alpha);
+           state.Add(BETA, beta);
+           return state;
+        }
+
+        public override void restore(JObject state)
+        {
+            alpha = (Double)state.GetValue(ALPHA);
+            beta = (Double)state.GetValue(BETA);
+        }
+
+        public override void init()
+        {
+            if (alpha % 1 < Utils.EPSILON && beta % 1 < Utils.EPSILON)
+                isIntegerType = true;
+            else
+                isIntegerType = false;
+        }
+
+        public override void validate()
+        {
+            if (alpha <= 0)
+                throw new CreateModelException("alpha <=0");
+            if (beta <= 0)
+                throw new CreateModelException("beta <=0");
+        }
+    }
+}
---
+++ Diplom/Data/Random/RandomGammaValue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Diplom.Data.Random
+{
+    class RandomGammaValue:AbstractRandomValue
+    {
+        private double param1;
+
+        public double param2;
+
+        public RandomGammaValue()
+        {
+        }
+        public override double nextValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override JObject store()
+        {
+            return base.store();
+        }
+
+        public override void restore(JObject state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
